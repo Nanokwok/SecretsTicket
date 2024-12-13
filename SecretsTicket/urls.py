dc67d05557ed906ef8f8f33d4a32a16506394840
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-12-14 04:09:52 +0700

    detail page

--- SecretsTicket/urls.py
+++ SecretsTicket/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "SecretsTicket"
@@ -6,4 +7,6 @@ app_name = "SecretsTicket"
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('ticket/', views.TicketView.as_view(), name='ticket'),
+    path('ticket/<int:pk>/', views.TicketDetailView.as_view(), name='ticket_detail'),
+    path('ticket/<int:pk>/delete/', views.TicketDeleteView.as_view(), name='ticket_delete'),
 ]
--- SecretsTicket/views.py
+++ SecretsTicket/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import TemplateView
 
 from SecretsTicket.models import Ticket
@@ -18,3 +19,24 @@ class TicketView(TemplateView):
             'tickets': tickets
         }
         return render(self.request, self.template_name, context)
+
+
+class TicketDetailView(TemplateView):
+    template_name = 'ticket_detail.html'
+
+    def get(self, *args, **kwargs):
+        ticket = Ticket.objects.get(pk=self.kwargs['pk'])
+        context = {
+            'ticket': ticket
+        }
+        return render(self.request, self.template_name, context)
+
+
+class TicketDeleteView(View):
+    def post(self, request, pk):
+        try:
+            ticket = Ticket.objects.get(pk=pk)
+            ticket.delete()
+            return redirect('SecretsTicket:ticket')
+        except Ticket.DoesNotExist:
+            return redirect('SecretsTicket:ticket')
