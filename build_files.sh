f1ebbe3f8b121177bc4e6df96e1987568dbbfb3f
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-12-14 06:08:57 +0700

    add echo

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo Starting build process...
+
 pip install -r requirements.txt
 
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
 python3.9 manage.py collectstatic --noinput
+
+echo Build process completed successfully.
