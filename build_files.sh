fce71bc5b517cbfb2c2b14b61a3283c805d481f0
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-12-14 05:54:49 +0700

    fix

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,4 @@ pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
-#creat superuser from environment variables
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$ADMIN_NAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python3.9 manage.py shell
-
 python3.9 manage.py collectstatic --noinput
