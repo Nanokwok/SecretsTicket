6d77e887bba14fefc0369a46a638a652114481d9
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-12-13 07:47:06 +0700

    home

---
+++ SecretsTicket/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SecretsticketConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SecretsTicket'
---
+++ SecretsTicket/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Ticket(models.Model):
+    """Model for a ticket."""
+    ticket_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, default='No title')
+    description = models.CharField(max_length=255, default='No description')
+
